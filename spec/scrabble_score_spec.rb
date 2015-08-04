fe356dd0dfa5c7a25f4d6e77cc1d8b9fa6b4c869
Evan Clough & Leah Klamkin <student@epicodus.com>
2015-08-04 12:28:17 -0700

    Split word into array of letters.

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -5,4 +5,8 @@ describe('String#scrabble_score') do
   it("returns a scrabble score for a letter") do
     expect("z".scrabble_score()).to(eq(10))
   end
+
+  it("takes a String and splits it into an array") do
+    expect("word".scrabble_score()).to(eq(["w", "o", "r", "d"]))
+  end
 end
