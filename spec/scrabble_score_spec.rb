ca0fc76df058044078677f40f1abf273b2ad5817
Evan Clough & Leah Klamkin <student@epicodus.com>
2015-08-04 14:06:35 -0700

    Final version.

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -6,16 +6,13 @@ describe('String#scrabble_score') do
     expect("z".scrabble_score()).to(eq(10))
   end
 
-  it("takes a String and splits it into an array") do
-    expect("word".scrabble_score()).to(eq(["w", "o", "r", "d"]))
-  end
+  # it("takes a String and splits it into an array") do
+  #   expect("word".scrabble_score()).to(eq(["w", "o", "r", "d"]))
+  # end
 
-  # it("runs a loop on the array to determine the score of the word") do
-  #   expect(["w", "o", "r", "d"].scrabble_score()).to(eq(8))
-
-  it("retrieves first letter of a word and fetches scrabble score") do
-    expect("word".scrabble_score()).to(eq(4))
-  end
+  # it("retrieves first letter of a word and fetches scrabble score") do
+  #   expect("word".scrabble_score()).to(eq(4))
+  # end
 
   it("adds the scores of all letters in a word") do
     expect("word".scrabble_score()).to(eq(8))
