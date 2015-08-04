f138e4d2a5757a8ed76f7ee8415dfdc159c88fc6
Evan Clough & Leah Klamkin <student@epicodus.com>
2015-08-04 13:49:00 -0700

    retrieves first letter of a word and fetches scrabble score.

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -9,4 +9,11 @@ describe('String#scrabble_score') do
   it("takes a String and splits it into an array") do
     expect("word".scrabble_score()).to(eq(["w", "o", "r", "d"]))
   end
+
+  # it("runs a loop on the array to determine the score of the word") do
+  #   expect(["w", "o", "r", "d"].scrabble_score()).to(eq(8))
+
+  it("retrieves first letter of a word and fetches scrabble score") do
+    expect("word".scrabble_score()).to(eq(4))
+  end
 end
