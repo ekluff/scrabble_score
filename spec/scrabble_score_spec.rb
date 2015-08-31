1772d20d06532a7e42d2cc171a86d2fc9d0ce7a2
Evan Clough <clough.evan@gmail.com>
2015-08-31 11:27:50 -0700

    Delete specs for early development steps. Specs now fail because of buildout.

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -6,14 +6,6 @@ describe('String#scrabble_score') do
     expect("z".scrabble_score()).to(eq(10))
   end
 
-  # it("takes a String and splits it into an array") do
-  #   expect("word".scrabble_score()).to(eq(["w", "o", "r", "d"]))
-  # end
-
-  # it("retrieves first letter of a word and fetches scrabble score") do
-  #   expect("word".scrabble_score()).to(eq(4))
-  # end
-
   it("adds the scores of all letters in a word") do
     expect("word".scrabble_score()).to(eq(8))
   end
